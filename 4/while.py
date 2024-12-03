d76f0fdfbc737dda85a5d6f768b59bae29f3d668
Florin Toma <florin.toma@me.com>
2024-12-03 14:35:24 +0100

    V01 - 03/12/24

---
+++ 4/while.py
@@ -0,0 +1,31 @@
+# WHILE - è un ciclo infinito di operazioni tutte uguali finché una condizione è verificata
+
+'''
+# ESERCIZIO INIZIO
+
+# Ciclo while e condizioni if per richiedere all’utente di inserire tre numeri pari negativi.
+# Il programma continua fino a quando l’utente non inserisce correttamente tre numeri che soddisfano il criterio.
+
+numeri = []  # Lista per memorizzare i numeri validi
+
+while len(numeri) < 3:
+    numero = input(f"Inserisci tre numeri pari negativi ({len(numeri) + 1}/3): ")
+
+    if numero.lstrip('-').isdigit(): # controllo se l'input è un numero
+        numero = int(numero)
+        if numero < 0 and numero % 2 == 0: # controllo se il numero è pari e negativo
+            numeri.append(numero) # aggiungo numero alla lista
+        else:
+            print("Il numero deve essere pari e negativo. Riprova.")
+    else:
+        print("Inserire un numero valido. Riprova.")
+
+print(f"Hai inserito correttamente i numeri pari negativi: {numeri}")
+
+# ESERCIZIO FINE
+'''
+
+i = 0
+while i < 6:
+    print(i)
+    i += 1
