04a026809fbe2edae0e3cdd6c8dfe9c8b0d5aa48
Florin Toma <florin.toma@me.com>
2024-12-03 15:07:34 +0100

    V03 - 03/12/24

--- 4/while.py
+++ 4/while.py
@@ -1,12 +1,13 @@
+from icecream import ic
 # WHILE - è un ciclo infinito di operazioni tutte uguali finché una condizione è verificata
 # break - serve a fermare il ciclo
 # continue - passa alla prossima iterazione salta il controllo (if x == 3)
-# else -
+# else - è identico allo if
 
 # break
 x = 0
 while x < 6:
-    print(x)
+    ic(x)
     if x == 3:
         break # quando 3 == 3 cliclo fermato dal break
     x += 1
@@ -17,5 +18,13 @@ while y < 6:
     y += 1
     if y == 3:
         continue # passa alla prossima iterazione ma non stampa "3"
-    print(y)
+    ic(y)
 
+# else
+
+z = 0
+while z < 6:
+    ic(z)
+    z += 1
+else: # non appena finita tutta l'iterazione posso stampare
+    ic("Ho finito!")
