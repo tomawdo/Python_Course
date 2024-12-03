de474d3925f56958f05946d972cabdc67ffff825
Florin Toma <florin.toma@me.com>
2024-12-03 16:24:03 +0100

    V07 - 03/12/24

---
+++ 4/for.py
@@ -0,0 +1,14 @@
+# il ciclo for si usa per iterare una sequenza
+# break, continue, else
+
+# Lista
+
+lista_citta = ["Torino", "Roma", "Napoli"]
+stringa = "anguria"
+# per ogni elemento singolo (città) in lista città fai qualcosa
+for citta in lista_citta:
+   print()
+
+x = [0,1,2,3,4,5,6,7]
+for x in range(6):
+    print(x)
--- 5/es_while.py
+++ 5/es_while.py
@@ -1,19 +1,17 @@
-# 1 - Scrivi un programma che chiede all'utente
-# di inserire un numero intero. Continua a chiedere all'utente
+# 1 - Scrivi un programma che continua a chiedere all'utente
 # di inserire un numero finché non viene inserito un numero pari.
 
 while True:
-    numero = int(input("Inserisci un numero: "))
+    numero = int(input("Inserisci un numero pari: "))
     if numero % 2 == 0:
         break
 
-# 2 - Scrivi un programma che stampi i numeri da 1 a 100
 
+# 2 - Scrivi un programma che stampi i numeri da 1 a 100
 n = 1
 while n <= 100:
     print(n)
     n += 1
-
 
 
 # 3 - scrivi un programma che chieda all'utente di inserire un
@@ -92,13 +90,3 @@ while True:
         break
     else:
         print("Riprova!")
-
-
-
-
-
-
-
-
-
-
