9c98acfa2f5629df234344a2195ecc717747b9ac
Florin Toma <40062893+tomawdo@users.noreply.github.com>
2024-12-04 15:11:32 +0100

    Update esercizi.py

--- 6/esercizi.py
+++ 6/esercizi.py
@@ -1,1 +1,22 @@
 # test
+dati = ["Angela", 51, "Maria", 12, "Dario", 34, "Marco", 34]
+print(dati[0], dati[1])
+
+targhe = ["AB345CV", "FR678AM", "ER772GD"]
+print(targhe)
+
+numeri = [10, 20, 30 ,40]
+print(numeri[3])
+
+a = 1
+b = 5
+c = 3
+
+VarBool = a > b
+print(VarBool)
+
+anni = [2024, 2025, 2209, 1988, 2007]
+frutta = ["mela", "pera", "uva", "anguria"]
+dati = [37, 52.5, "camicia"]
+
+print(dati[-2])
