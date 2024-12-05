be598b096e52886b07917b2fb7abe706f4d46f92
Florin Toma <40062893+tomawdo@users.noreply.github.com>
2024-12-05 15:43:48 +0100

    Update main.py

--- 05-12-24/main.py
+++ 05-12-24/main.py
@@ -1,12 +1,3 @@
-vocali = "aeiou"
-listavocali = list(vocali)
-# print(listavocali)
-
-listavocali.insert(len(listavocali), "W")
-print(listavocali)
-
-
-import random
-
-r1 = random.randint(5, 15)
-print(r1)
+- creare una lista vuota
+- generare un numero random compreso tra 1 e 20
+- tramite ciclo for, aggiungere alla lista una quantità di numeri generati casualmente pari al primo numero casuale generato.
