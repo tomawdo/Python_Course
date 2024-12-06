4b5d5c2a7e14a0aa6ab5a473759ac0709e1a1101
Florin Toma <40062893+tomawdo@users.noreply.github.com>
2024-12-06 13:56:54 +0100

    Update esercizi.py

--- 06-12-24/esercizi.py
+++ 06-12-24/esercizi.py
@@ -1,48 +1,67 @@
-# chiedete di inserire tre cibi e salvai in una lista
+import random
 
+# chiedete di inserire tre cibi e salvai in una lista
+'''
+# metodo 1
 cibi = []
 for cibo in range(3):
     domanda = input("Inserisci un cibo: ")
     cibi.append(domanda)
 print(cibi)
 
+# metodo 2
 cibi = [input("Inserisci un cibo: ") for _ in range(3)]
 print(cibi)
 
+
+for n in range(3):
+    x = random.randint(1, 100)
+    print(x)
+
+# stampa tutti i numeri compresi tra 1 e 20
+# per ogni numero nel range
+for n in range(1, 21):
+    print(n)
+
+# inserire una frase e stampare tutte le lettere della frase
+
+frase = input("Inserisci una frase")
+for f in frase:
+    print(f)
+
+
 # chiedi all'utente di inserire 3 elementi in una lista e stampa ogni lettera di ogni elemento della lista
-listaElementi = []
+# listaElementi = []
+
 for elemento in range(3):
     richiesta = input("Inserisci elemento: ")
+#   listaElementi.append(richiesta)
     for se in richiesta:
         print(se)
-        
-        
-        
+
+
 listaElementi = []
 
-# Chiedi all'utente di inserire 3 elementi nella lista
 for i in range(3):
     richiesta = input("Inserisci elemento: ")
     listaElementi.append(richiesta)
 
-# Stampa ogni parola in spelling
 for elemento in listaElementi:
-    print(f"\nParola: {elemento}")  # Introduce la parola
+    print(f"\nParola: {elemento}")
     for lettera in elemento:
-        print(lettera)
+        print(lettera, end=" | ")
+'''
 
-# chiediamo di inserire due numeri
-# stampiamo tutti i numeri compresi nel range indicato dall'utente
-
-# Chiediamo all'utente di inserire due numeri
+# Chiediamo all'utente di inserire due numeri la cui funzione sia definire il range
 numero1 = int(input("Inserisci il primo numero: "))
 numero2 = int(input("Inserisci il secondo numero: "))
 
-# Determiniamo l'intervallo corretto (minimo e massimo)
-minimo = min(numero1, numero2)
-massimo = max(numero1, numero2)
+n_min = min(numero1, numero2)
+n_max = max(numero1, numero2)
 
-# Stampiamo tutti i numeri compresi nel range
-print("I numeri compresi tra", minimo, "e", massimo, "sono:")
-for numero in range(minimo, massimo + 1):
-    print(numero)
+print(f"I numeri compresi {n_min} e {n_max} sono: ")
+
+for numero in range(n_min, n_max + 1):
+    print(f"{numero}")
+
+
