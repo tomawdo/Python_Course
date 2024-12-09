0f673091d67b6cb7074a33090194e0992e796691
Florin Toma <40062893+tomawdo@users.noreply.github.com>
2024-12-09 12:52:27 +0100

    Update esercizi.py

--- 09-12-24/esercizi.py
+++ 09-12-24/esercizi.py
@@ -1,1 +1,5 @@
-print("!")
+# Scrivi un programma che chieda all'utente che svoga le seguenti operazioni:
+# - trova la lunghezza della stringa
+# - determina il carattere minimo e massimo in base all'ordine alfabetico
+# - ordina i caratteri della stringa in ordine alfabetico
+# - usa str per convertire un numero intero come stringa
