5d123dd7196083cb7dbb0b18c943c1f5f141a489
Florin Toma <40062893+tomawdo@users.noreply.github.com>
2024-12-13 12:35:04 +0100

    Update main.py

--- 13-12-24/main.py
+++ 13-12-24/main.py
@@ -15,3 +15,18 @@ if posizioni_vocali:
     print("Le vocali si trovano alle seguenti posizioni:", posizioni_vocali)
 else:
     print("Non ci sono vocali nella parola inserita.")
+    
+    
+    
+Quando utilizzate uno sportello bancario automatico (ATM, automatic teller machine) con la vostra
+carta, dovete usare un numero identificativo personale (PIN, personal identification number) per
+poter accedere al vostro conto. Se un utente sbaglia tre volte l’inserimento del PIN, la macchina
+trattiene la carta e la blocca. Nell’ipotesi che il PIN dell’utente sia “1234”, scrivete un programma
+che chieda all’utente di digitare il PIN, consentendo al massimo tre tentativi e agendo in questo
+modo:
+- se l’utente inserisce il numero corretto, visualizzate il messaggio “Il tuo PIN è corretto” e
+terminate il programma;
+- se l’utente inserisce un numero sbagliato, visualizzate il messaggio “Il tuo PIN non è corretto”
+e, se avete chiesto il PIN meno di tre volte, chiedetelo di nuovo;
+- se l’utente inserisce un numero sbagliato per tre volte, visualizzate il messaggio “La banca ha
+bloccato la tua carta” e terminate il programma
