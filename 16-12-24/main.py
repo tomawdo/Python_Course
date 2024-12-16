23f84d2fde16dc6ebba631d26e3eaec5d767295f
Florin Toma <40062893+tomawdo@users.noreply.github.com>
2024-12-16 13:19:41 +0100

    Update main.py

--- 16-12-24/main.py
+++ 16-12-24/main.py
@@ -5,3 +5,9 @@ print(d["name"])
 
 # Access using get()
 print(d.get("name"))
+
+d = {"name": "Alice", 1: "Python", (1, 2): [1, 2, 4]}
+
+# Iterare sulle chiavi e stampare chiave e valore
+for key in d:
+    print(f"Chiave: {key}, Valore: {d[key]}")
