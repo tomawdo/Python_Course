e7b68e026ec99ae52ae86b4549dd195b8388ada6
Florin Toma <40062893+tomawdo@users.noreply.github.com>
2024-12-16 13:34:04 +0100

    Update main.py

--- 16-12-24/main.py
+++ 16-12-24/main.py
@@ -11,3 +11,7 @@ d = {"name": "Alice", 1: "Python", (1, 2): [1, 2, 4]}
 # Iterare sulle chiavi e stampare chiave e valore
 for key in d:
     print(f"Chiave: {key}, Valore: {d[key]}")
+
+
+# ESERCIZIO in PyTHON - creare un dizionario con chiave - valore (nome - voto universitario) max 10 alunni
+# visualizzare a video la media dei voti di tutti gli alunni
