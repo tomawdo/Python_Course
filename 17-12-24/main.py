c030263b111b4872d5ce59de9924236469459817
Florin Toma <40062893+tomawdo@users.noreply.github.com>
2024-12-17 12:39:58 +0100

    Update main.py

--- 17-12-24/main.py
+++ 17-12-24/main.py
@@ -6,19 +6,11 @@
 
 # Esercizio 3 - scrivi una funzione (separat dal codice main) che richieda una serie di numeri (lista) e stampi il valore max
 
-# Definizione della funzione
-def stampa_valore_massimo(lista_numeri):
-    if len(lista_numeri) == 0:
-        print("La lista è vuota. Non è possibile determinare il valore massimo.")
-    else:
-        valore_massimo = max(lista_numeri)
-        print(f"Il valore massimo è: {valore_massimo}")
 
-# Codice principale
-if __name__ == "__main__":
-    # Input dell'utente
-    numeri = input("Inserisci una serie di numeri separati da spazi: ").split()
-    numeri = [float(numero) for numero in numeri]  # Converte gli input in numeri
-    stampa_valore_massimo(numeri)
-
-
+# Esercizio 4 - scrivi la versione della funzione len() senza usare len()
+# inizializzo la variabile contatore a 0
+# definisco una variabile con un argomento
+# creo un ciclo for che analizzi tutti gli elementi presenti nell'argomento della variabile
+# per ogni elemento presente nell'argoomento della variabile aggiungo 1 e riassegno contatore
+# uso return per restituire il valore di contatore
+# test funzione su una stringa inserita dall'utente
