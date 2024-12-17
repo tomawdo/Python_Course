60545ee2ca043a751bc3c5e4cf8e396c5006b8e7
Florin Toma <40062893+tomawdo@users.noreply.github.com>
2024-12-17 12:57:35 +0100

    Update main.py

--- 17-12-24/main.py
+++ 17-12-24/main.py
@@ -1,10 +1,40 @@
-# Esercizio 1: 
+from funzioni import *
+
+'''
+# Esercizio 1
+operatore = input("Inserisci un operatore (+, -, *, /): ")
+n1 = int(input("Inserisci il primo numero: "))
+n2 = int(input("Inserisci il secondo numero: "))
+
+if operatore == "+":
+    somma(n1, n2)
+elif operatore == "-":
+    sottrazione(n1, n2)
+elif operatore == "*":
+    moltiplicazione(n1, n2)
+elif operatore == '/':
+    if n2 != 0:
+        divisione(n1, n2)
+    else:
+        print("Errore: divisione per zero.")
+else:
+    print("Operazione non valida.")
 
 
-# Esercizio 2: chiedi all'utente di inserire una lista di parole (max 5) tramite una funzione stampo un elenco di parole che inziano con la "s"
+# Eserzio 2
+parole = input("Inserisci 5 parole separate da uno spazio: ").split()[:5]
+parole_con_esse = filtra_parole_con_esse(parole)
+
+print("Le parole che iniziano con la lettera 's' sono:")
+for parola in parole_con_esse:
+    print(parola)
 
 
-# Esercizio 3 - scrivi una funzione (separat dal codice main) che richieda una serie di numeri (lista) e stampi il valore max
+# Esercizio 3 - scrivi una funzione che richieda una serie di numeri (lista) e stampi il valore max
+numeri = input("Inserisci 5 numeri separati da spazi: ").split()[:5]
+numeri = [float(numero) for numero in numeri]
+numeri_max(numeri)
+'''
 
 
 # Esercizio 4 - scrivi la versione della funzione len() senza usare len()
@@ -14,16 +44,5 @@
 # per ogni elemento presente nell'argoomento della variabile aggiungo 1 e riassegno contatore
 # uso return per restituire il valore di contatore
 # test funzione su una stringa inserita dall'utente
-
-# Definizione della funzione per calcolare la lunghezza
-def lunghezza(elemento):
-    contatore = 0  # Inizializzo il contatore a 0
-    for _ in elemento:  # Scorro ogni elemento dell'argomento passato
-        contatore += 1  # Incremento il contatore di 1 per ogni elemento
-    return contatore  # Restituisco il valore del contatore
-
-# Test della funzione su una stringa inserita dall'utente
 stringa = input("Inserisci una stringa: ")
-print(f"La lunghezza della stringa è: {lunghezza(stringa)}")
-
-
+print(f"Lunghezza: {lunghezza(stringa)}")
