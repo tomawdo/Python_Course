dd2c3be5d442b08500ff3ae65740f144c268e210
Florin Toma <40062893+tomawdo@users.noreply.github.com>
2024-12-17 10:41:36 +0100

    Update main.py

--- 17-12-24/main.py
+++ 17-12-24/main.py
@@ -1,1 +1,36 @@
-print("Ciao!")
+def sommatoria():
+    print(n1 + n2)
+sommatoria()
+
+def sottrazione():
+    print(n1 - n2)
+sottrazione()
+
+def moltiplicazione():
+    print(n1 * n2)
+moltiplicazione()
+
+def divisione():
+    if n2 != 0:
+        print(f"{n1 / n2}")
+    else:
+        print("Errore: divisione per zero.")
+divisione()
+
+operatore = input("Inserisci un operatore (+, -, *, /): ")
+n1 = int(input("Inserisci il primo numero"))
+n2 = int(input("Inserisci il secondo numero"))
+
+if operatore == "+":
+        sommatoria(operatore)
+    elif operatore == "-":
+        sottrazione(operatore)
+    elif operatore == "*":
+        moltiplicazione(operatore)
+    elif operatore == '/':
+        if n2 != 0:
+            divisione(operatore)
+        else:
+            print("Errore: divisione per zero.")
+    else:
+        print("Operazione non valida.")
