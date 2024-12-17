6baaa2e85095dc7f1e1684d0460fa76f3bfbb9d0
Florin Toma <40062893+tomawdo@users.noreply.github.com>
2024-12-17 12:16:56 +0100

    Update main.py

--- 17-12-24/main.py
+++ 17-12-24/main.py
@@ -1,40 +1,4 @@
-def sommatoria():
-    print(n1 + n2)
-sommatoria()
-
-def sottrazione():
-    print(n1 - n2)
-sottrazione()
-
-def moltiplicazione():
-    print(n1 * n2)
-moltiplicazione()
-
-def divisione():
-    if n2 != 0:
-        print(f"{n1 / n2}")
-    else:
-        print("Errore: divisione per zero.")
-divisione()
-
-operatore = input("Inserisci un operatore (+, -, *, /): ")
-n1 = int(input("Inserisci il primo numero"))
-n2 = int(input("Inserisci il secondo numero"))
-
-if operatore == "+":
-        sommatoria(operatore)
-    elif operatore == "-":
-        sottrazione(operatore)
-    elif operatore == "*":
-        moltiplicazione(operatore)
-    elif operatore == '/':
-        if n2 != 0:
-            divisione(operatore)
-        else:
-            print("Errore: divisione per zero.")
-    else:
-        print("Operazione non valida.")
-
+# Esercizio 1: 
 
 
 # Esercizio 2: chiedi all'utente di inserire una lista di parole (max 5) tramite una funzione stampo un elenco di parole che inziano con la "s"
