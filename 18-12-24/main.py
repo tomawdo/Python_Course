0c51cebe3a45badeea03d3656468a75f5bce1386
Florin Toma <40062893+tomawdo@users.noreply.github.com>
2024-12-18 13:00:06 +0100

    Update main.py

--- 18-12-24/main.py
+++ 18-12-24/main.py
@@ -1,1 +1,3 @@
-print("Hello World")
+# creare una lista la cui dimensione è decisa dall'utente sia il minimo e sia il massimo dei numeri
+# popolare la lista con numeri generati random che rientrano nel range definito
+# es: lunghezza lista: 5, num_minimo, num_massimo
