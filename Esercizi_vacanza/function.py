41865a7d97ef33bc9c0abb4811d0acdff44c7ce6
Florin Toma <40062893+tomawdo@users.noreply.github.com>
2025-01-07 13:53:11 +0100

    Update function.py

--- Esercizi_vacanza/function.py
+++ Esercizi_vacanza/function.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def sfida1():
     print("Scrivi un programma che permetta a un negoziante di calcolare lo sconto. "
           "Il programma prende in input il prezzo iniziale e lo sconto applicato, restituendo il prezzo finale scontato.")
@@ -131,6 +133,80 @@ nel silenzio andiam."""
         if i % 2 == 0:
             print(f"\n{riga}")
 
+def sfida7():
+    print("Scrivi un programma che ti permetta di inserire data e ora corrente e che calcoli quanto tempo manca al Natale \n")
+    #data_corrente = datetime.now()
+    data_corrente = input("Inserisci la data di oggi: ")
+    print(data_corrente)
+    natale = datetime(data_corrente.year, 12, 25)
+    if data_corrente > natale:
+        natale = datetime(data_corrente.year + 1, 12, 25)
+    tempo_rimanente = natale - data_corrente
+
+    print(f"Tempo rimanente fino a Natale: {tempo_rimanente}")
+
+def sfida8():
+    print("Scrivi un programma che calcoli il volume di una casa di zenzero, l'utente deve inserire base, altezza e profondità")
+
+    base = int(input("Base in mm: "))
+    altezza = int(input("Altezza in mm: "))
+    profondità = int(input("Profondità in mm: "))
+
+    result = base * altezza * profondità
+    print(f"Il volume è {result} mm")
+
+def sfida9():
+    print("Scrivi un programma che permetta di dividere automaticamente il costo di una cena"
+          "tra un gruppo di amici. Il programma prende in input il costo totale della cena e "
+          "il numero di partecipanti e restituisce la quota procapite")
+    
+    costo = int(input("Inserisci il costo totale della cena: "))
+    amici = int(input("In quanti eravate? "))
+    print(f"La quota per ciscuno è di {costo / amici:.2f}")
+
+def sfida10():
+    filastrocca = input("Inserisci filastrocca: ")
+    lettera = input("Inserisci la lettera da contare: ")
+
+    result = filastrocca.lower().count(lettera.lower())
+    print(f"La lettera '{lettera}' si ripete {result} volte nella filastrocca.")
+
+def sfida11():
+    print("Scrivi un programma che ti permetta di registrarti come aiutante di Babbo Natale. Se l'utente ha meno di 18 anni non può essere accettato")
+    anno_nascita = int(input("Inserisci l'anno di nascita: "))
+
+    anno = datetime.now().year
+    result = anno - anno_nascita
+    if result > 18:
+        print(f"Ottimo, poiché hai {result}, sei un lil Babbo.")
+    else:
+        print("Purtroppo sei minorenne.")
+
+def sfida12():
+    print("Calcola lo stipendio di un elfo. Prendi in input le ore lavorate "
+          "mensilmente dall'elfo e la retribuzione oraria e restituisce lo stipendio mensile.")
+    ore_lavorate = int(input("Quante ore ha lavorato? "))
+    retribuzione_oraria = int(input("Quale è la paga oraria? "))
+    stipendio = ore_lavorate * retribuzione_oraria
+
+    print(f"Stipendio elfico: {stipendio:.2f}EUR")
+
+def sfida13():
+    print("Dato il peso massimo trasportabile dalla slitta di Babbo Natale (inserito dall'utente),"
+          "crea un programma che permetta di inserire n regali finchè non si raggiunge il numero"
+          "consentito")
+    nr_regali = int(input("Quanti regali può trasportare? "))
+    regali = []
+
+    for tot_reg in range(nr_regali):
+        regalo = int(input("Quanti regali vuoi trasportare? "))
+        regalo =+ regali.append
+
+def sfida14():
+    print("Scrivi un programma che calcoli la distanza percorsa da una palla di neve."
+          "L'utente deve poter inserire due input, m/s(metri al secondo) e s(secondi) impiegati per atterrare.")
+
+
 
 if __name__ == "__main__":
     sfida1(),
@@ -138,4 +214,12 @@ if __name__ == "__main__":
     sfida3(),
     sfida4(),
     sfida5(),
-    sfida6()
+    sfida6(),
+    sfida7(),
+    sfida8(),
+    sfida9(),
+    sfida10(),
+    sfida11(),
+    sfida12(),
+    sfida13(),
+    sfida14()
