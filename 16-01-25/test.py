71f0563b656d2422900112efae1b2ee24137dd9f
Florin Toma <florin.toma@me.com>
2025-01-20 15:50:08 +0100

    updates 20-01-25

--- 16-01-25/test.py
+++ 16-01-25/test.py
@@ -1,15 +1,13 @@
 # confrontare ottetto per ottetto l'indirizzo IP con la subnet mask
 # stampa finale: gli host sono sulla stessa rete oppure Glo host non sono sulla stessa rete
 
-# 150 & 255 = ?
-# 169 & 255 = ?
-# 3 & 255 = ?
-# 8 & 0 = ?
-
-
+# 150 & 255 =
+# 169 & 255 =
+# 3 & 255 =
+# 8 & 0 =
 
 host_a = "150.169.3.8"
-host_b = "150.169.3.2"
+host_b = "150.169.2.5"
 sm = "255.255.255.0"
 
 parti_host_a = list(map(int, host_a.split('.')))
